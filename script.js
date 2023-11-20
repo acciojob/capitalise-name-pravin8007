e83f0d208698819b6e9a434831236639acf66bbb
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-20 11:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function capitalizeName() {
+        // Get the input field by its id
+        var inputField = document.getElementById('fname');
+
+        // Convert the content to uppercase
+        var uppercaseName = inputField.value.toUpperCase();
+
+        // Update the input field with the uppercase content
+        inputField.value = uppercaseName;
+    }
+
+    // Add event listener to the input field
+    document.getElementById('fname').addEventListener('blur', capitalizeName);
